ba4d59f8c2c7e0cd5835833f8311bb855f7a6bd9
ahmedRguei <ahmed.rguei@softtodo.com>
2017-05-19 18:09:47 +0200

    Third commit

---
+++ typo3temp/Cache/Code/fluid_template/Candidateresume_Candidate_layout_Default_22ab8d088c7f3f0a9c54ef6a085d22ba1aced4f0.php
@@ -0,0 +1,50 @@
+<?php
+class FluidCache_Candidateresume_Candidate_layout_Default_22ab8d088c7f3f0a9c54ef6a085d22ba1aced4f0 extends \TYPO3\CMS\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getVariableContainer() {
+	// @todo
+	return new \TYPO3\CMS\Fluid\Core\ViewHelper\TemplateVariableContainer();
+}
+public function getLayoutName(\TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$currentVariableContainer = $renderingContext->getTemplateVariableContainer();
+$self = $this;
+
+return NULL;
+}
+public function hasLayout() {
+return FALSE;
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$currentVariableContainer = $renderingContext->getTemplateVariableContainer();
+
+$output0 = '';
+
+$output0 .= '
+<div class="tx-candidateresume">
+	';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper
+$arguments1 = array();
+$arguments1['section'] = 'main';
+$arguments1['partial'] = NULL;
+$arguments1['arguments'] = array (
+);
+$arguments1['optional'] = false;
+$renderChildrenClosure2 = function() {return NULL;};
+
+$output0 .= TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper::renderStatic($arguments1, $renderChildrenClosure2, $renderingContext);
+
+$output0 .= '
+</div>';
+
+
+return $output0;
+}
+
+
+}
+#1495213509    1321      
